3202292316af377819a5654a174099f3ff3bf8bd
Caesar11SL <97268174+Caesar11SL@users.noreply.github.com>
2022-04-30 12:16:27 -0700

    heroku should be working

--- db/connection.js
+++ db/connection.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 const mongoURI =
   process.env.NODE_ENV === 'production'
     ? process.env.DB_URL
-    : "mongodb+srv://test:test@cluster0.ga9qe.mongodb.net/Simpsons?retryWrites=true&w=majority";
+    : process.env.DEV_DB_URL;
 
-  mongoose.connect("mongodb+srv://test:test@cluster0.ga9qe.mongodb.net/Simpsons?retryWrites=true&w=majority")
+  mongoose.connect(mongoURI)
   .then((instance) => console.log(`Connected to db: ${instance.connections[0].name}`)
   )
   .catch((error) => console.log('Connection failed!', error));
